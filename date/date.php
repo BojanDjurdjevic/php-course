78296aa9b11afaa36f3f59be0b8dc46c767dd5ff
BojanDjurdjevic <pininfarina164@gmail.com>
2026-09-19 22:08:16 +0200

    New date functions added in date.php file

--- date/date.php
+++ date/date.php
@@ -55,4 +55,158 @@ echo "<b>checkdate()</b> " . "</br>" . "</br>";
 
 var_dump(checkdate(12, 8, 1986));
 
+echo "</br>" . "</br>";
+//-------------------------------
+
+// The getdate(timestamp - optional) function returns date/time information of a timestamp or the current local date/time.
+
+echo "<b>getdate()</b></br>";
+print_r(getdate());
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+echo "<b>gmdate()</b></br>";
+
+// The gmdate(format, timestamp (optional)) function formats a GMT/UTC date and time, and returns the formatted date string.
+
+// Prints the day
+echo gmdate("l") . "<br>";
+
+// Prints the day, date, month, year, time, AM or PM
+echo gmdate("l jS \of F Y h:i:s A");
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+// The microtime() function returns the current Unix timestamp with microseconds.
+
+/**
+ * 	Returns the string "microsec sec" by default, 
+ * where sec is the number of seconds since the Unix Epoch (0:00:00 January 1, 1970 GMT), 
+ * and microsec is the microseconds part. 
+ * If the return_float parameter is set to TRUE, 
+ * it returns a float representing the current time in seconds since the Unix epoch accurate to the nearest microsecond
+ */
+echo "<b>microtime()</b></br>";
+
+echo microtime();
+echo "</br>" . "</br>";
+//-------------------------------
+
+// hrtime()
+
+echo "<b>hrtime()</b></br>";
+
+echo hrtime(true);
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+//The date_default_timezone_set(timezone) function sets the default timezone used by all date/time functions in the script.
+echo "<b>date_default_timezone_set()</b></br>";
+
+date_default_timezone_set("Europe/Belgrade");
+echo date_default_timezone_get() . "</br>";
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+//The timezone_identifiers_list(what, country - both optional) returns an indexed array containing all timezone identifiers.
+
+/**
+ *   1 = AFRICA
+ *   2 = AMERICA
+ *   4 = ANTARCTICA
+ *   8 = ARCTIC
+ *   16 = ASIA
+ *   32 = ATLANTIC
+ *   64 = AUSTRALIA
+ *   128 = EUROPE
+ *   256 = INDIAN
+ *   512 = PACIFIC
+ *   1024 = UTC
+ *   2047 = ALL
+ *   4095 = ALL_WITH_BC
+ *   4096 = PER_COUNTRY
+ */
+
+echo "<b>date_default_timezone_set()</b></br>";
+
+print_r(timezone_identifiers_list(128));
+echo "</br>";
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+// The date_parse(date) function returns an associative array with detailed information about a specified date.
+
+echo "<b>date_parse()</b></br>";
+
+print_r(date_parse("2013-05-01 12:30:45.5"));
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+// -------------------------------------------------------- DATE AS OBJECT---------------------------------------------------
+
+// DateTime je mutable i njegove metode mogu promeniti postojeći objekat
+
+echo "new DateTime() class" . "</br>"; 
+
+$date = new DateTime();
+
+echo $date->format('Y-m-d H:i:s');
+echo "</br>" . "</br>";
+
+$date->modify("+1 week");
+echo $date->format('Y-m-d H:i:s');
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+// DateTimeInmutable() radi slično ali ne menja objekat
+
+$date = new DateTimeImmutable('2026-09-19');
+
+$newDate = $date->modify('+1 month');
+
+echo $date->format('Y-m-d') . "</br>";
+// 2026-09-19
+
+echo $newDate->format('Y-m-d');
+// 2026-10-19
+
+echo "</br>" . "</br>";
+
+/**
+ *  DateTime
+ *   modify() menja postojeći objekat
+ *  DateTimeImmutable
+ *  modify() vraća novi objekat 
+ */
+//------------------------------
+
+// setDate()
+
+echo "<b>setDate()<b> </br>";
+
+$date = new DateTime();
+
+$date->setDate(2030, 3, 21);
+
+echo $date->format("d.m.Y");
+
+echo "</br>" . "</br>";
+//-------------------------------
+
+// setTime() - modifikuje vreme
+
+$date = new DateTime('2026-09-19');
+
+$date->setTime(14, 30); // h, m, mogu se dodati i s
+
+echo $date->format('Y-m-d H:i:s');
+// 2026-09-19 14:30:00
+
 ?>
